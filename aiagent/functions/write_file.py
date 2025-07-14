00d64886b03c3d7382463dfdaa08aea40d04184a
van512 <evannesmith31@gmail.com>
2025-07-14 14:59:19 +0200

    added write_file and corresponding tests.py

---
+++ aiagent/functions/write_file.py
@@ -0,0 +1,16 @@
+import os
+def write_file(working_directory, file_path, content):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_file = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not target_file.startswith(abs_working_dir):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    
+    try:
+        # Create the file if it doesn't exist
+        with open(target_file, "w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+
+    except Exception as e:
+        return f'Error: Could not create or write to file "{file_path}": {str(e)}'
--- aiagent/tests.py
+++ aiagent/tests.py
@@ -1,16 +1,21 @@
 # from subdirectory.filename import function_name
 #from functions.get_files_info import get_files_info
-from functions.get_file_content import get_file_content
+#from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 #"""
 #my tests
 def test():
+    #write_file tests
+    print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+    print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+
     # get_file_content tests
     #print(get_file_content("calculator", "lorem.txt"))
-    print(get_file_content("calculator", "main.py"))
-    print(get_file_content("calculator", "pkg/calculator.py"))
-    print(get_file_content("calculator", "/bin/cat")) 
-
+    #print(get_file_content("calculator", "main.py"))
+    #print(get_file_content("calculator", "pkg/calculator.py"))
+    #print(get_file_content("calculator", "/bin/cat")) 
 
     # get_files_info tests
     #print(get_files_info("calculator", "main.py")) #works
